18a067a23a1392fae7fdec1741f0b85b59158a70
Roberto Butti <roberto.butti@gmail.com>
2023-10-01 17:54:04 +0200

    home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,43 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://some-drops-of-php.vercel.app/',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Some drops of PHP',
+			customCss: [
+				'./src/styles/custom.css'
+			],
+			editLink: {
+				baseUrl: 'https://github.com/roberto-butti/some-drops-of-php/edit/main',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/roberto-butti/some-drops-of-php',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+					label: '📔 Intro',
+					autogenerate: { directory: '00-intro' },
+					collapsed: false
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '🧑‍💻 System',
+					autogenerate: { directory: '01-system' },
+					collapsed: true
+				},
+				{
+					label: '📚 Array',
+					autogenerate: { directory: '02-array' },
+					collapsed: true
+				},
+				{
+					label: '[ 🧑‍💻 Sources ]',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Markdown', link: 'https://github.com/roberto-butti/some-drops-of-php/tree/main/src/content/docs' },
+						{ label: 'Examples', link: 'https://github.com/roberto-butti/some-drops-of-php/tree/main/examples' },
+					],
+					collapsed: true
 				},
 			],
 		}),
