d851474e308f55d81854f751a81414ee6a729464
Roberto Butti <roberto.butti@gmail.com>
2023-10-05 14:44:04 +0200

    examples for chapter 2

---
+++ examples/02-arr/02-arr-join.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 'Concatenates String with -- : '.implode('--', ['aa', 'bb', 'cc']).PHP_EOL; // "aa--bb--cc"
+echo 'Concatenates Strings : '.implode(' ', ['How', 'are', 'you', '?']).PHP_EOL; // "How are you ?"
+
+echo 'Without separator: '.implode(['a', 'b', 'c']).PHP_EOL; // "abc"
+echo 'Not just string: '.implode('_', ['a', 1, true]).PHP_EOL;
---
+++ examples/02-arr/04-arr-in_array.php
@@ -0,0 +1,37 @@
+<?php
+
+$array = ['a', 'b', 'c'];
+$elementToFind = 'b';
+
+$inArray = in_array($elementToFind, $array);
+
+var_dump($inArray);
+
+// with strict
+$array = ['1', 'b', 'c'];
+$elementToFind = 1;
+$inArray = in_array($elementToFind, $array);
+var_dump($inArray);
+$inArray = in_array($elementToFind, $array, true);
+var_dump($inArray);
+
+// side effect of no strict
+
+$array = [
+    true,
+    false,
+    0,
+    999,
+    null,
+    'something',
+];
+
+in_array(111, $array); // true ??? :)
+in_array(false, $array); // true
+in_array(1, $array); // true ??? :)
+in_array('another string', $array); // true , because the true
+printf('empty array is in array: %b'.PHP_EOL, in_array([], $array)); // true, because the false
+
+$array = ['My Test', 'second', 'third'];
+in_array('my tesT', $array); // false because in_array is case sensitive
+in_array(strtolower('my tesT'), array_map('strtolower', $array)); // true
