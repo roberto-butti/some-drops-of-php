c98928bb94866c636f7b794ae2ed1e55b9cddde1
Roberto Butti <roberto.butti@gmail.com>
2023-10-08 18:22:45 +0200

    Update 08-fil-file.php

--- examples/06-file/08-fil-file.php
+++ examples/06-file/08-fil-file.php
@@ -1,3 +1,21 @@
 <?php
 
+// retrieving the content of the current PHP file
 print_r(file(__FILE__));
+
+// you can retrieve also remote file
+print_r(file('https://cdn.wsform.com/wp-content/uploads/2020/06/color_web_safe.csv'));
+
+// if you need to set some header, you can use the "context"
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => "Accept-language: en\r\n" .
+            "Cookie: foo=bar\r\n"
+    )
+);
+$context = stream_context_create($opts);
+print_r(file(
+    filename: 'https://cdn.wsform.com/wp-content/uploads/2020/06/color_web_safe.csv',
+    context: $context
+));
